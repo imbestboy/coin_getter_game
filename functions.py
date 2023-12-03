4700cda19dbcedc27f49afeb71aa3df30fd97684
SaJaD (ImBestBoy) <Mohamadsajad2012@gmail.com>
2023-12-03 18:59:17 +0330

    feat: add speed changer
    add speed changer slider and label to show speed

--- functions.py
+++ functions.py
@@ -11,3 +11,15 @@ def change_theme(theme: str) -> None:
         customtkinter.set_appearance_mode(theme)
     else:
         raise ValueError("theme should be in 'dark' , 'light' and 'system'")
+
+
+def show_spaceship_speed_value(
+    speed_value: float, spaceship_speed_label: customtkinter.CTkLabel
+) -> None:
+    """show_spaceship_speed_value change spaceship speed label next to spaceship slider for end user with every slider move
+
+    Arguments:
+        speed_value {float} -- spaceship speed
+        spaceship_speed_label {customtkinter.CTkLabel} -- label to show spaceship speed
+    """
+    spaceship_speed_label.configure(text=int(speed_value))
--- main.py
+++ main.py
@@ -29,7 +29,23 @@ customtkinter.CTkLabel(
     text=f"Spaceship Speed (default : {config.spaceship_default_speed}) : ",
     font=config.normal_font,
 ).grid(column=0, row=1)
-
+spaceship_speed = customtkinter.IntVar(value=config.spaceship_default_speed)
+spaceship_speed_slider = customtkinter.CTkSlider(
+    main_menu_window,
+    from_=1,
+    to=20,
+    number_of_steps=19,
+    variable=spaceship_speed,
+    command=lambda value: functions.show_spaceship_speed_value(
+        value, spaceship_speed_label
+    ),
+    width=150,
+)
+spaceship_speed_slider.grid(column=1, row=1)
+spaceship_speed_label = customtkinter.CTkLabel(
+    main_menu_window, text=config.spaceship_default_speed, font=config.bold_font
+)
+spaceship_speed_label.grid(column=2, row=1)
 
 # -- keep running
 main_menu_window.mainloop()
